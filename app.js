6b5d01c1ca179713bb5a1c8dce1480f223f8edcd
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-14 17:45:09 -0400

    Include functionality Realized console.log was not working because event listener needed DOM in all caps

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-document.addEventListener('DomContentLoaded', () => {
+// event listener that runs after entire HTML has loaded
+document.addEventListener('DOMContentLoaded', () => {
+// allowing js to access our grid
+  const grid = document.querySelector('.grid')
+// access all 200 divs bruh >.<
+// Array.from() allows each grid to have an expecific index
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+// telling js the size of our grid
+  const width = 10
+
+  console.log(squares)
 })
+
+// console.log('butts')
