50493af149f0961109da12adeacfe10a76398b38
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-15 13:36:56 -0400

    add L tetromino shapes using array index and width of board

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
 // telling js the size of our grid
   const width = 10
 
+
+//Tetrominoes
+//each array index represents a part of the tetromino shape
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0, width]
+  ]
+
+
   console.log(squares)
 })
 
