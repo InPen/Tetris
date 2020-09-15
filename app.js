bf10d3ed45dcaf5fa40ac3a7f62e2065ebddca2c
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-15 13:54:12 -0400

    Add o and i tetromino shapes index*width

--- app.js
+++ app.js
@@ -36,6 +36,19 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width, width+1, width*2+1]
   ]
 
+  const oTetromino = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+  ]
+
+  const iTetromino = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
 
   console.log(squares)
 })
