07f36ff21217a32c4496dd2a068dccdfdcd9a340
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2020-09-15 12:30:03 -0400

    Include more functionality

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
 // access all 200 divs bruh >.<
 // Array.from() allows each grid to have an expecific index
   let squares = Array.from(document.querySelectorAll('.grid div'))
+// acces span to display score
+  const scoreDisplay = document.getElementById('#score')
+// button functionality
+  const startButton = document.getElementById('#startButton')
 // telling js the size of our grid
   const width = 10
 
